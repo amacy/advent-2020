45e1baa8ecf6ed8b0d15ac107ab7474174184513
Aaron Macy <amacy@paypal.com>
2020-12-01 13:11:08 -0500

    day 1, part 2: solved

--- lib/day_01.rb
+++ lib/day_01.rb
@@ -12,6 +12,28 @@ class Day01
     end
   end
 
+  def self.part_2(input=File.new("config/day_01.txt").read)
+    list = _parse_input(input)
+    x_index = 0
+    loop do
+      x = list[x_index]
+      raise if x.nil?
+
+      list[1..-1].each_with_index do |y, y_index|
+        if x + y < 2020
+
+          list[y_index + 1..-1].each do |z|
+            if x + y + z == 2020
+              return x * y * z
+            end
+          end
+        end
+      end
+
+      x_index += 1
+    end
+  end
+
   def self._parse_input(input)
     input.split("\n").map(&:to_i)
   end
--- spec/day_01_spec.rb
+++ spec/day_01_spec.rb
@@ -1,20 +1,42 @@
 require_relative "../lib/day_01"
+require "pry"
 
 RSpec.describe Day01 do
-  it "calculates the correct answer for the sample input" do
-    input = <<~INPUT
-      1721
-      979
-      366
-      299
-      675
-      1456
-    INPUT
+  context "part 1" do
+    it "calculates the correct answer for the sample input" do
+      input = <<~INPUT
+        1721
+        979
+        366
+        299
+        675
+        1456
+      INPUT
 
-    expect(Day01.part_1(input)).to eq 514579
+      expect(Day01.part_1(input)).to eq 514579
+    end
+
+    it "calculates the correct answer for the input file" do
+      expect(Day01.part_1).to eq 138379
+    end
   end
 
-  it "calculates the correct answer for the input file" do
-    expect(Day01.part_1).to eq 138379
+  context "part 2" do
+    it "calculates the correct answer for the sample input" do
+      input = <<~INPUT
+        1721
+        979
+        366
+        299
+        675
+        1456
+      INPUT
+
+      expect(Day01.part_2(input)).to eq 241861950
+    end
+
+    it "calculates the correct answer for the input file" do
+      expect(Day01.part_2).to eq 85491920
+    end
   end
 end
