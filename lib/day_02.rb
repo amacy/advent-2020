2c3c58b5237b7105220cf0a5ba19941d9c88592b
Aaron Macy <amacy@paypal.com>
2020-12-02 11:54:33 -0500

    day 2, part 2: solved

--- lib/day_02.rb
+++ lib/day_02.rb
@@ -3,30 +3,40 @@ class Day02
     list = _parse_input(input)
 
     list.inject(0) do |sum, instructions|
-      sum += 1 if valid_password?(*instructions)
+      sum += 1 if valid_part_1_password?(*instructions)
       sum
     end
   end
 
-  def self.valid_password?(range, letter, password)
-    range.include?(password.count(letter))
+  def self.valid_part_1_password?(range, letter, password)
+    (range[0]..range[1]).include?(password.count(letter))
   end
 
-  # def self.part_2(input=File.new("config/day_02.txt").read)
-  #   list = _parse_input(input)
-  # end
+  def self.part_2(input=File.new("config/day_02.txt").read)
+    list = _parse_input(input)
+
+    list.inject(0) do |sum, instructions|
+      sum += 1 if valid_part_2_password?(*instructions)
+      sum
+    end
+  end
+
+  def self.valid_part_2_password?(indices, letter, password)
+    x = password[indices[0] - 1]
+    y = password[indices[1] - 1]
+
+    if x == letter
+      y != letter
+    else
+      y == letter
+    end
+  end
 
   def self._parse_input(input)
     input.split("\n").map do |line|
       instructions = line.split(" ")
-
-      # parse range
-      range = instructions[0].split("-")
-      instructions[0] = range[0].to_i..range[1].to_i
-
-      # strip colon
+      instructions[0] = instructions[0].split("-").map(&:to_i)
       instructions[1] = instructions[1][0]
-
       instructions
     end
   end
