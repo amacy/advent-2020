e0f0eab7a312b20b77dbdc66b5deb20357406169
amacy <aaronmacy@gmail.com>
2020-12-05 12:30:00 -0500

    day 5, part 1: solved

---
+++ lib/day_05.rb
@@ -0,0 +1,75 @@
+class Day05
+  ROWS = 128
+  COLUMNS = 8
+
+  def self.part_1(input=File.new("config/day_05.txt").read)
+    tickets = _parse_input(input)
+    seat_ids = []
+
+    tickets.each do |ticket|
+      first_row = 0
+      last_row = ROWS - 1
+      first_column = 0
+      last_column = COLUMNS - 1
+
+      7.times do
+        first_row, last_row = _half(ticket.shift, first_row, last_row)
+      end
+      raise "error calculating rows" if first_row != last_row
+
+      3.times do
+        first_column, last_column = _half(ticket.shift, first_column, last_column)
+      end
+      raise "errors calculating columns" if first_column != last_column
+
+      seat_ids << first_row * 8 + first_column
+    end
+
+    seat_ids.max
+  end
+
+  def self._half(instruction, x, y)
+    difference = y - x
+    remainder = (difference / 2)
+
+    if ["F", "L"].include?(instruction)
+      if difference == 1
+        [x, x]
+      else
+        [x, x + remainder]
+      end
+    elsif ["B", "R"].include?(instruction)
+      if difference == 1
+        [y, y]
+      else
+        remainder += 1 if remainder % 2 != 0
+        [x + remainder, y]
+      end
+    else
+      raise "invalid input"
+    end
+  end
+
+  def self._parse_input(input)
+    input.split("\n").map do |ticket|
+      ticket.split("")
+    end
+  end
+end
+
+# 1, 64
+# 32, 64
+# 32, 48
+# 40, 48
+# 44, 48
+# 44, 46
+# 44, 45
+#
+#
+# 0, 63
+# 31, 63
+# 31, 47
+# 39, 47
+# 43, 47
+# 43, 45
+# 43, 44
---
+++ spec/day_05_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../lib/day_05"
+require "pry"
+
+RSpec.describe Day05 do
+  context "part 1" do
+    it "calculates the correct answer for the sample input" do
+      input = <<~INPUT
+        FBFBBFFRLR
+        BFFFBBFRRR
+        BFFFBBFRRR
+        BBFFBBFRLL
+      INPUT
+
+      expect(Day05.part_1(input)).to eq 820
+    end
+
+    it "calculates the correct answer for the input file" do
+      expect(Day05.part_1).to eq 913
+    end
+  end
+end
