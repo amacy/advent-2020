37b4d53d4fb1877fb06573e014d724db785d8114
Aaron Macy <amacy@paypal.com>
2020-12-11 17:59:15 -0500

    day 8, part 2: solved

--- lib/day_08.rb
+++ lib/day_08.rb
@@ -3,12 +3,53 @@ class Day08
 
   def self.part_1(input=File.new("config/day_08.txt").read)
     instructions = _parse_input(input)
+    _execute_instructions(instructions)[0]
+  end
+
+  def self.part_2(input=File.new("config/day_08.txt").read)
+    original_instructions = _parse_input(input)
+
+    index = 0
+
+    loop do
+      original_instructions.each do |node|
+        node.executed = false
+      end
+
+      instructions = original_instructions.dup
+
+      if instructions[index].operation == "nop"
+        replaced_node = instructions[index]
+        instructions[index] = Node.new("jmp", replaced_node.argument)
+        index += 1
+      elsif instructions[index].operation == "jmp"
+        replaced_node = instructions[index]
+        instructions[index] = Node.new("nop", replaced_node.argument)
+        index += 1
+      else
+        index += 1
+        next
+      end
+
+      # binding.pry if index == 8
+      acc, offset = _execute_instructions(instructions)
+
+      if offset == original_instructions.length
+        return acc
+      end
+    end
+  end
+
+  def self._execute_instructions(instructions)
     offset = 0
     acc = 0
+
     loop do
       node = instructions[offset]
 
-      break if node.executed
+      if node.nil? || node.executed
+        return [acc, offset]
+      end
 
       case node.operation
       when "nop"
@@ -21,11 +62,6 @@ class Day08
       end
       node.executed = true
     end
-
-    acc
-  end
-
-  def self.part_2(input=File.new("config/day_08.txt").read)
   end
 
   def self._parse_input(input)
