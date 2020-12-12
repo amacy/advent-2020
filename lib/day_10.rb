94f144d135342974addbc450a10d55fcb652f451
amacy <aaronmacy@gmail.com>
2020-12-11 22:49:17 -0500

    wip

--- lib/day_10.rb
+++ lib/day_10.rb
@@ -1,5 +1,7 @@
+require "set"
+
 class Day10
-  def self.part_1(input=File.new("config/day_10.txt").read, preamble=25)
+  def self.part_1(input=File.new("config/day_10.txt").read)
     adapters = _parse_input(input)
     one_jolt_diff = 0
     three_jolt_diff = 1
@@ -21,7 +23,33 @@ class Day10
     one_jolt_diff * three_jolt_diff
   end
 
-  def self.part_2(input=File.new("config/day_10.txt").read, preamble=25)
+  def self.part_2(input=File.new("config/day_10.txt").read)
+    adapters = SortedSet.new(_parse_input(input))
+    last_voltage = 0
+    optional = []
+
+    loop do
+      selected = adapters.select do |voltage|
+        (last_voltage + 1..last_voltage + 3).include?(voltage)
+      end
+
+      puts selected.inspect
+      if selected.length > 1
+        optional += selected[0..-2]
+      end
+
+      last_voltage = selected.last
+      puts last_voltage
+      break if last_voltage == adapters.max
+    end
+
+    # none of optional & all of optional
+    count = 2
+    binding.pry
+    1.upto(optional.length - 1).each do |combo_count|
+      count += optional.combination(combo_count).count
+    end
+    count
   end
 
   def self._parse_input(input)
--- spec/day_10_spec.rb
+++ spec/day_10_spec.rb
@@ -26,16 +26,63 @@ RSpec.describe Day10 do
     end
   end
 
-  # context "part 2" do
-  #   it "calculates the correct answer for the sample input" do
-  #     input = <<~INPUT
-  #     INPUT
-  #
-  #     expect(Day10.part_2(input, 5)).to eq 62
-  #   end
-  #
-  #   it "calculates the correct answer for the input file" do
-  #     expect(Day10.part_2).to eq 20532569
-  #   end
-  # end
+  context "part 2" do
+    it "calculates the correct answer for the sample input" do
+      input_1 = <<~INPUT
+        16
+        10
+        15
+        5
+        1
+        11
+        7
+        19
+        6
+        12
+        4
+      INPUT
+
+      expect(Day10.part_2(input_1)).to eq 8
+
+      input_2 = <<~INPUT
+        28
+        33
+        18
+        42
+        31
+        14
+        46
+        20
+        48
+        47
+        24
+        23
+        49
+        45
+        19
+        38
+        39
+        11
+        1
+        32
+        25
+        35
+        8
+        17
+        7
+        9
+        4
+        2
+        34
+        10
+        3
+      INPUT
+
+      expect(Day10.part_2(input_2)).to eq 19208
+    end
+
+    it "calculates the correct answer for the input file" do
+      expect(Day10.part_2).to eq 20532569
+    end
+  end
 end
