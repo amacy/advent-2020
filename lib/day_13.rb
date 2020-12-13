8a9b17bdf5375f6ab0e40f3aeb62552b8ce27efc
amacy <aaronmacy@gmail.com>
2020-12-13 12:09:43 -0500

    day 13, part 1: solved

---
+++ lib/day_13.rb
@@ -0,0 +1,34 @@
+class Day13
+  def self.part_1(input=File.new("config/day_13.txt").read)
+    timestamp, buses = _parse_input(input)
+    buses.delete("x")
+    delta = buses.max
+    best_bus = nil
+
+    buses.each do |bus|
+      candidate = (0..timestamp + bus).step(bus).last - timestamp
+      puts candidate
+      if candidate < delta
+        delta = candidate
+        best_bus = bus
+      end
+    end
+
+    delta * best_bus
+  end
+
+  def self.part_2(input=File.new("config/day_13.txt").read)
+  end
+
+  def self._parse_input(input)
+    timestamp, buses = input.split("\n")
+    buses = buses.split(",").map do |bus|
+      if bus == "x"
+        bus
+      else
+        bus.to_i
+      end
+    end
+    [timestamp.to_i, buses]
+  end
+end
