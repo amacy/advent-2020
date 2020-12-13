04ba3c570fbc83a13a68b6f7f824d903e91f3313
amacy <aaronmacy@gmail.com>
2020-12-13 15:31:06 -0500

    works for sample input but doesn't scale to file

--- lib/day_13.rb
+++ lib/day_13.rb
@@ -7,7 +7,6 @@ class Day13
 
     buses.each do |bus|
       candidate = (0..timestamp + bus).step(bus).last - timestamp
-      puts candidate
       if candidate < delta
         delta = candidate
         best_bus = bus
@@ -17,7 +16,39 @@ class Day13
     delta * best_bus
   end
 
-  def self.part_2(input=File.new("config/day_13.txt").read)
+  def self.part_2(input=File.new("config/day_13.txt").read, starting_point=100000000000000)
+    _, buses = _parse_input(input)
+    max = buses.select { |bus| bus != "x" }.max
+    index_of_max = buses.index(max)
+    if index_of_max > 0
+      reversed_first_section = buses[0..index_of_max - 1].reverse
+    else
+      reversed_first_section = []
+    end
+    second_section = buses[index_of_max + 1..-1]
+
+    (starting_point..).step(max) do |timestamp|
+      if _check_first_section_timestamps(reversed_first_section, timestamp) &&
+        _check_second_section_timestamps(second_section, timestamp)
+        return timestamp - reversed_first_section.length
+      end
+    end
+  end
+
+  def self._check_first_section_timestamps(reversed_first_section, timestamp)
+    reversed_first_section.each_with_index do |bus, index|
+      next if bus == "x"
+      return false if (timestamp - index - 1) % bus != 0
+    end
+    true
+  end
+
+  def self._check_second_section_timestamps(second_section, timestamp)
+    second_section.each_with_index do |bus, index|
+      next if bus == "x"
+      return false if (timestamp + index + 1) % bus != 0
+    end
+    true
   end
 
   def self._parse_input(input)
--- spec/day_13_spec.rb
+++ spec/day_13_spec.rb
@@ -18,20 +18,46 @@ RSpec.describe Day13 do
   end
 
   context "part 2" do
-    # it "calculates the correct answer for the sample input" do
-    #   input = <<~INPUT
-    #     F10
-    #     N3
-    #     F7
-    #     R90
-    #     F11
-    #   INPUT
-    #
-    #   expect(Day13.part_2(input)).to eq 286
-    # end
-    #
-    # it "calculates the correct answer for the input file" do
-    #   expect(Day13.part_2).to eq 13340
-    # end
+    it "calculates the correct answer for the sample input" do
+      input_1 = <<~INPUT
+        939
+        7,13,x,x,59,x,31,19
+      INPUT
+      expect(Day13.part_2(input_1, 0)).to eq 1068781
+
+      input_2 = <<~INPUT
+        939
+        17,x,13,19
+      INPUT
+      expect(Day13.part_2(input_2, 0)).to eq 3417
+
+      input_3 = <<~INPUT
+        939
+        67,7,59,61
+      INPUT
+      expect(Day13.part_2(input_3, 0)).to eq 754018
+
+      input_4 = <<~INPUT
+        939
+        67,x,7,59,61
+      INPUT
+      expect(Day13.part_2(input_4, 0)).to eq 779210
+
+      input_5 = <<~INPUT
+        939
+        67,7,x,59,61
+      INPUT
+      expect(Day13.part_2(input_5, 0)).to eq 1261476
+
+      input_6 = <<~INPUT
+        939
+        1789,37,47,1889
+      INPUT
+      expect(Day13.part_2(input_6, 0)).to eq 1202161486
+    end
+
+    it "calculates the correct answer for the input file" do
+      expect(Day13.part_2).to eq 13340
+    end
   end
 end
