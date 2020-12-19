a2b6ca8a7face75e5d98757c6e426bbba8df39bd
amacy <aaronmacy@gmail.com>
2020-12-18 21:43:33 -0500

    solution for single parens

--- lib/day_18.rb
+++ lib/day_18.rb
@@ -1,15 +1,46 @@
 require "set"
 
-class Day14
-  def self.part_1(input=File.new("config/day_14.txt").read)
+# this one really needs recursion
+class Day18
+  def self.part_1(input=File.new("config/day_18.txt").read)
+    expressions = _parse_input(input)
+
+    expressions.map do |expression|
+      values = expression.split(/ /)
+      current_total = values.shift.to_i
+      next_value = nil
+      symbol = nil
+      sum = 0
+
+      loop do
+        symbol = values.shift.to_sym
+        next_value = values.shift
+        if next_value.include?("(")
+          next_value = next_value[1..-1].to_i
+          loop do
+            next_symbol = values.shift.to_sym
+            next_next_value = values.shift
+            if next_next_value[-1] == ")"
+              next_value = next_value.send(
+                next_symbol,
+                next_next_value[0..-2].to_i,
+              )
+              break
+            else
+              next_value = next_value.send(next_symbol, next_next_value.to_i)
+            end
+          end
+        end
+        current_total = current_total.send(symbol, next_value.to_i)
+        break current_total if values.length == 0
+      end
+    end.sum
   end
 
-  def self.part_2(input=File.new("config/day_14.txt").read)
+  def self.part_2(input=File.new("config/day_18.txt").read)
   end
 
   def self._parse_input(input)
-    input.split("\n").map do |line|
-      line.split(" = ")
-    end
+    input.split("\n")
   end
 end
--- spec/day_18_spec.rb
+++ spec/day_18_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Day18 do
   context "part 1" do
     it "calculates the correct answer for the sample input" do
       expect(Day18.part_1("1 + 2 * 3 + 4 * 5 + 6")).to eq 71
-      expect(Day18.part_1("1 + (2 * 3) + (4 * (5 + 6))")).to eq 51
       expect(Day18.part_1("2 * 3 + (4 * 5)")).to eq 26
       expect(Day18.part_1("5 + (8 * 3 + 9 + 3 * 4 * 3)")).to eq 437
+      expect(Day18.part_1("1 + (2 * 3) + (4 * (5 + 6))")).to eq 51
       expect(Day18.part_1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")).to eq 12240
       expect(Day18.part_1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")).to eq 13632
     end
@@ -17,20 +17,20 @@ RSpec.describe Day18 do
     end
   end
 
-  context "part 2" do
-    it "calculates the correct answer for the sample input" do
-      input = <<~INPUT
-        mask = 000000000000000000000000000000X1001X
-        mem[42] = 100
-        mask = 00000000000000000000000000000000X0XX
-        mem[26] = 1
-      INPUT
-
-      expect(Day18.part_2(input)).to eq 208
-    end
-
-    it "calculates the correct answer for the input file" do
-      expect(Day18.part_2).to eq 3885232834169
-    end
-  end
+  # context "part 2" do
+  #   it "calculates the correct answer for the sample input" do
+  #     input = <<~INPUT
+  #       mask = 000000000000000000000000000000X1001X
+  #       mem[42] = 100
+  #       mask = 00000000000000000000000000000000X0XX
+  #       mem[26] = 1
+  #     INPUT
+  #
+  #     expect(Day18.part_2(input)).to eq 208
+  #   end
+  #
+  #   it "calculates the correct answer for the input file" do
+  #     expect(Day18.part_2).to eq 3885232834169
+  #   end
+  # end
 end
